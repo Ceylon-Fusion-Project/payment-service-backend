2c4be79affcd0b99b6aed0cc807ed5a2e8c444b9
gayeshakavi <kavindi12416@usci.ruh.ac.lk>
2025-02-01 17:02:40 +0530

    Add Stripe webhook handling

--- src/main/java/com/ceylon_fusion/payment_service/service/serviceIMPL/StripeServiceIMPL.java
+++ src/main/java/com/ceylon_fusion/payment_service/service/serviceIMPL/StripeServiceIMPL.java
@@ -2,8 +2,12 @@ package com.ceylon_fusion.payment_service.service.serviceIMPL;
 
 import com.ceylon_fusion.payment_service.service.StripeService;
 import com.stripe.Stripe;
+import com.stripe.exception.SignatureVerificationException;
 import com.stripe.exception.StripeException;
+import com.stripe.model.Event;
 import com.stripe.model.PaymentIntent;
+import com.stripe.model.StripeObject;
+import com.stripe.net.Webhook;
 import com.stripe.param.PaymentIntentCreateParams;
 import com.stripe.param.RefundCreateParams;
 import lombok.RequiredArgsConstructor;
@@ -86,7 +90,7 @@ public class StripeServiceIMPL implements StripeService {
 
     @Override
     public void validateWebhookSignature(String payload, String sigHeader) {
-        // Will implement webhook signature validation
+
     }
 
     @Override
@@ -127,4 +131,79 @@ public class StripeServiceIMPL implements StripeService {
         }
 
     }
+
+    @Override
+    public void handleStripeEvent(String eventType, StripeObject stripeObject) {
+        switch (eventType) {
+            case "payment_intent.succeeded":
+                handlePaymentIntentSucceeded((PaymentIntent) stripeObject);
+                break;
+            case "payment_intent.payment_failed":
+                handlePaymentIntentFailed((PaymentIntent) stripeObject);
+                break;
+            case "payment_intent.canceled":
+                handlePaymentIntentCanceled((PaymentIntent) stripeObject);
+                break;
+            case "payment_method.attached":
+                handlePaymentMethodAttached(stripeObject);
+                break;
+            case "payment_method.detached":
+                handlePaymentMethodDetached(stripeObject);
+                break;
+            case "refund.created":
+                handleRefundCreated(stripeObject);
+                break;
+            case "refund.failed":
+                handleRefundFailed(stripeObject);
+                break;
+            case "refund.updated":
+                handleRefundUpdated(stripeObject);
+                break;
+            default:
+                log.info("Unhandled event type: {}", eventType);
+        }
+    }
+
+    private void handlePaymentIntentSucceeded(PaymentIntent paymentIntent) {
+        log.info("Payment succeeded: {}", paymentIntent.getId());
+        // Process successful payment
+    }
+
+    private void handlePaymentIntentFailed(PaymentIntent paymentIntent) {
+        log.error("Payment failed: {}", paymentIntent.getId());
+        // Process failed payment
+    }
+
+    private void handlePaymentIntentCanceled(PaymentIntent paymentIntent) {
+        log.info("Payment canceled: {}", paymentIntent.getId());
+        // Process canceled payment
+    }
+
+    private void handlePaymentMethodAttached(StripeObject stripeObject) {
+        log.info("Payment method attached: {}", stripeObject.toJson());
+        // Handle payment method attachment
+    }
+
+    private void handlePaymentMethodDetached(StripeObject stripeObject) {
+        log.info("Payment method detached: {}", stripeObject.toJson());
+        // Handle payment method detachment
+    }
+
+    private void handleRefundCreated(StripeObject stripeObject) {
+        log.info("Refund created: {}", stripeObject.toJson());
+        // Process refund creation
+    }
+
+    private void handleRefundFailed(StripeObject stripeObject) {
+        log.error("Refund failed: {}", stripeObject.toJson());
+        // Handle refund failure
+    }
+
+    private void handleRefundUpdated(StripeObject stripeObject) {
+        log.info("Refund updated: {}", stripeObject.toJson());
+        // Process refund update
+
+    }
+
+
 }
